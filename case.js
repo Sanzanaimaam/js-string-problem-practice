76ca98545c25438a71837c5d0cd301c7501b3f40
Sanzanaimaam <sanzanaimaam@gmail.com>
2023-02-03 08:53:53 +0600

    case

---
+++ case.js
@@ -0,0 +1,12 @@
+const userName='BlackPiNk';
+const userInput='bLacKPinK';
+const newInput =userInput.toLowerCase();
+console.log(newInput);
+const newName =userName.toLowerCase();
+console.log(newName);
+if(newName===newInput){
+    console.log('valid')
+}
+else{
+    console.log('invalid')
+}
