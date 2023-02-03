e7a5016e42f405e73e5543865d37e06000552903
Sanzanaimaam <sanzanaimaam@gmail.com>
2023-02-03 13:43:07 +0600

    commit

---
+++ largest.js
@@ -0,0 +1,15 @@
+function tallestNumber(array){
+    let tallest=array[0];
+    for(let i=0; i<array.length; i++){
+        const index=i;
+        const elements=array[index];
+        if(elements>tallest){
+            tallest=elements;
+        }
+    }
+    return tallest;
+
+}
+const number=[134,765,355,245,666];
+const toFunction=tallestNumber(number);
+console.log(toFunction);
